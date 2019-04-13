dde239e7e4b1fc5004c3f57d82b9d65c75b7a761
Phuc Tran <glup3.tran@gmail.com>
2019-04-13 17:01:06 +0200

    add home page with pic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Home />
         <Footer />
       </div>
     );
