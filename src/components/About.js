6c9a4f35086123767c1cefc6dda1cddb057142da
Phuc Tran <glup3.tran@gmail.com>
2019-04-13 23:09:18 +0200

    replace about lorem with content

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,26 @@ import React from 'react'
 function About() {
   return (
     <div className="block container">
-      <div className="">
+      <div>
         <h1>About</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, ab repudiandae rem tenetur accusamus at animi neque exercitationem corrupti veniam alias? Recusandae blanditiis autem veniam ipsa itaque incidunt nulla sapiente id iusto reprehenderit debitis omnis, mollitia vero accusamus soluta eum vel voluptate! Dicta distinctio libero, voluptatem aut nisi, quisquam quibusdam ratione pariatur cumque laborum modi enim et amet quas cum repellendus neque fugiat corporis adipisci minus asperiores. Eaque numquam incidunt natus necessitatibus repellat exercitationem ut perferendis quis nesciunt voluptatem unde assumenda sint, consectetur ipsum, ducimus minima totam eveniet error voluptate fugiat reprehenderit praesentium odio iste illo? Mollitia reprehenderit, doloremque ullam aspernatur nam pariatur culpa, fuga tempora alias cum, debitis blanditiis? A aliquid veritatis rem ex ratione omnis dolore suscipit reprehenderit distinctio fugiat. Rerum sunt itaque nam repellat quasi? Ipsa accusamus suscipit iste est quos doloremque numquam at asperiores dolores architecto fugiat culpa quis, nam laborum quidem repudiandae perspiciatis eaque veniam debitis, voluptatem odit? Corrupti ab architecto nam aut cumque nostrum accusantium reprehenderit consequatur similique qui laborum error itaque perferendis, ipsum molestias? Pariatur ipsam aut vel sapiente similique illo quod laudantium provident facilis, dolorem odit quia reiciendis possimus enim aspernatur doloribus nostrum eveniet sunt aperiam laboriosam numquam quas. Doloribus, quo ullam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat consequatur ducimus nulla sit minima repellat provident! Beatae, cupiditate? Iusto doloremque praesentium modi id voluptate sed eligendi in cum delectus dolores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam possimus doloribus eveniet ratione autem velit veniam cum quae alias earum voluptas, quam ullam laudantium totam voluptatum sequi reprehenderit nesciunt excepturi!</p>
+        <h5>Phuc Tran | 17 years old | Student</h5>
+        <p>I'm currently attending the best school for higher technical education: <a href="http://www.spengergasse.at" target="blank">Spengergasse</a></p>
+        <p>
+          In the first years we learn object orientated programming basics and patterns.
+          We start with simple console applications, continue with desktop application
+          and graduate with mobile and web applications. After 5 years of school we are
+          skilled enough to solve all kinds of problems.
+        </p>
+        <p>
+          I love to code in my spare time, but there also a lot of other things I do, like
+          practising taekwondo, running, skating, reading books / mangas / light novels, watching animes, playing computer games, studying japanese, ...
+        </p>
+        <blockquote>
+          The best code is no code at all.<br />
+          A clever person solves a problem. A wise person avoids it.<br />
+          DRY - Don't Repeat Yourself<br />
+          Any fool can write code that a computer can understand. Good programmers write code that humans can understand.
+        </blockquote>
       </div>
     </div>
   )
