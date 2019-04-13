2a3e7339c75c54dc4fe21f21e871d6d5250eb4d3
Phuc Tran <glup3.tran@gmail.com>
2019-04-14 00:35:19 +0200

    highlight about block

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,19 @@ function About() {
       <div>
         <h1>About</h1>
         <p className="flow-text">Phuc Tran | 17 years old | Student</p>
-        <p className="flow-text">I'm currently attending the best school for higher technical education: <a href="http://www.spengergasse.at" target="blank">Spengergasse</a></p>
         <p className="flow-text">
-          In the first years we learn object orientated programming basics and patterns.
+          I'm currently attending the <span className="blue lighten-4">best school</span> for higher technical education: <a href="http://www.spengergasse.at" target="blank">Spengergasse</a>
+        </p>
+        <p className="flow-text">
+          In the first years we learn <span className="blue lighten-4">object orientated programming</span> basics and patterns.
           We start with simple console applications, continue with desktop application
-          and graduate with mobile and web applications. After 5 years of school we are
-          skilled enough to solve all kinds of problems.
+          and graduate with <span className="blue lighten-4">mobile and web</span> applications. After 5 years of school we are
+          skilled enough to <span className="blue lighten-4">solve</span> all kinds of <span className="blue lighten-4">problems</span>.
         </p>
         <p className="flow-text">
           I love to code in my spare time, but there are also a lot of other things I do, like
-          practising taekwondo, running, skating, reading books / mangas / light novels, watching animes, playing computer games, studying japanese, ...
+          practising <span className="blue lighten-4">taekwondo</span>, running, skating, reading books / mangas / light novels,
+          watching animes, playing <span className="blue lighten-4">computer games</span>, studying japanese, ...
         </p>
         <blockquote className="flow-text">
           The best code is no code at all.
