ea3856727852e267924a32d1f5eac854fb8d4a75
Phuc Tran <glup3.tran@gmail.com>
2019-04-14 00:39:34 +0200

    highlight tech stack block

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -11,32 +11,33 @@ function Technologies() {
           <div className="col s6 m4 l3 xl2"><h5>JS</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>PHP</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>JQuery</h5></div>
-          <div className="col s6 m4 l3 xl2"><h5>ReactJS</h5></div>
+          <div className="col s6 m4 l3 xl2"><h5><span className="blue lighten-4">ReactJS</span></h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Angular</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Django</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Spring</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Bootstrap</h5></div>
-          <div className="col s6 m4 l3 xl2"><h5>Materialize</h5></div>
+          <div className="col s6 m4 l3 xl2"><h5><span className="blue lighten-4">Materialize</span></h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Wordpress</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Websockets</h5></div>
-          <div className="col s6 m4 l3 xl2"><h5>Java</h5></div>
+          <div className="col s6 m4 l3 xl2"><h5><span className="blue lighten-4">Java</span></h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Android</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>C#</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Python</h5></div>
-          <div className="col s6 m4 l3 xl2"><h5>Flutter</h5></div>
+          <div className="col s6 m4 l3 xl2"><h5><span className="blue lighten-4">Flutter</span></h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Github</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Bitbucket</h5></div>
-          <div className="col s6 m4 l3 xl2"><h5>MySQL</h5></div>
+          <div className="col s6 m4 l3 xl2"><h5><span className="blue lighten-4">MySQL</span></h5></div>
           <div className="col s6 m4 l3 xl2"><h5>PLSQL</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>PostgreSQL</h5></div>
+          <div className="col s6 m4 l3 xl2"><h5><span className="blue lighten-4">MongoDB</span></h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Firestore</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>NodeJS</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Express</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Bash</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Unity</h5></div>
-          <div className="col s6 m4 l3 xl2"><h5>GraphQL</h5></div>
+          <div className="col s6 m4 l3 xl2"><h5><span className="blue lighten-4">GraphQL</span></h5></div>
           <div className="col s6 m4 l3 xl2"><h5>REST</h5></div>
-          <div className="col s6 m4 l3 xl2"><h5>Docker</h5></div>
+          <div className="col s6 m4 l3 xl2"><h5><span className="blue lighten-4">Docker</span></h5></div>
           <div className="col s6 m4 l3 xl2"><h5>Microservices</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>NGINX</h5></div>
           <div className="col s6 m4 l3 xl2"><h5>CI/CD</h5></div>
