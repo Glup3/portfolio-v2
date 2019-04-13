16cd7c2d7575785d40b363f76b07e872f9ab0166
Phuc Tran <glup3.tran@gmail.com>
2019-04-13 16:39:02 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container center">
+        <p>Designed and built with <span className="red-text">♥</span> by Phuc.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
