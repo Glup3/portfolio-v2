52e15204ad4222c100c179868d83a571e6525856
Phuc Tran <glup3.tran@gmail.com>
2019-04-13 21:39:54 +0200

    fix classname error

--- src/components/contact/SubmitButton.js
+++ src/components/contact/SubmitButton.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function SubmitButton() {
   return (
-    <button class="btn waves-effect waves-light light-blue darken-1" type="submit" name="action">
+    <button className="btn waves-effect waves-light light-blue darken-1" type="submit" name="action">
       Submit
-      <i class="material-icons right">send</i>
+      <i className="material-icons right">send</i>
     </button>
   )
 }
