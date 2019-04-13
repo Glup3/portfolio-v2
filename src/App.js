aeba01436bf639c24545594bd43865b869d7a47b
Phuc Tran <glup3.tran@gmail.com>
2019-04-13 16:12:35 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <h1>Glup3</h1>
+      <div className="App">
+        <Navbar />
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import M from 'materialize-css';
+
+class Navbar extends Component {
+
+  componentDidMount() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    let options = {
+
+    };
+
+    M.Sidenav.init(sidenav, options);
+  }
+
+  render() {
+    return (
+      <header>
+        <div className="">
+          <nav className="nav-wrapper light-blue darken-1">
+            <div className="container">
+              <a href="#!" className="brand-logo">Glup3</a>
+              <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+              <ul className="right hide-on-med-and-down">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#home">About</a></li>
+                <li><a href="#home">Tech stack</a></li>
+                <li><a href="#home">Projects</a></li>
+                <li><a href="#home">Contact</a></li>
+              </ul>
+            </div>
+          </nav>
+
+          <ul id="mobile-demo" className="sidenav">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#home">About</a></li>
+            <li><a href="#home">Tech stack</a></li>
+            <li><a href="#home">Projects</a></li>
+            <li><a href="#home">Contact</a></li>
+          </ul>
+        </div>
+      </header>
+    )
+  }
+}
+
+export default Navbar
