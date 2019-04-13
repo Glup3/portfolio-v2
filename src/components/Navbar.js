8f1da7b58a9a7bb42c7923865cd1b715ed07ea75
Phuc Tran <glup3.tran@gmail.com>
2019-04-13 22:18:00 +0200

    make navbar transparent

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,29 +15,28 @@ class Navbar extends Component {
   render() {
     return (
       <header>
-        <div className="sticky">
-          <nav className="nav-wrapper light-blue darken-1">
+        <div className="navbar-fixed">
+          <nav className="nav-wrapper custom-transparent z-depth-0">
             <div className="container">
-              <a href="#!" className="brand-logo">Glup3</a>
-              <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+              <a href="#!" className="brand-logo black-text">Glup3</a>
+              <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons black-text">menu</i></a>
               <ul className="right hide-on-med-and-down">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#home">About</a></li>
-                <li><a href="#home">Tech stack</a></li>
-                <li><a href="#home">Projects</a></li>
-                <li><a href="#home">Contact</a></li>
+                <li><a href="#home" className="black-text">Home</a></li>
+                <li><a href="#home" className="black-text">About</a></li>
+                <li><a href="#home" className="black-text">Tech stack</a></li>
+                <li><a href="#home" className="black-text">Projects</a></li>
+                <li><a href="#home" className="black-text">Contact</a></li>
               </ul>
             </div>
           </nav>
-
-          <ul id="mobile-demo" className="sidenav">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#home">About</a></li>
-            <li><a href="#home">Tech stack</a></li>
-            <li><a href="#home">Projects</a></li>
-            <li><a href="#home">Contact</a></li>
-          </ul>
         </div>
+        <ul id="mobile-demo" className="sidenav">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#home">About</a></li>
+          <li><a href="#home">Tech stack</a></li>
+          <li><a href="#home">Projects</a></li>
+          <li><a href="#home">Contact</a></li>
+        </ul>
       </header>
     )
   }
