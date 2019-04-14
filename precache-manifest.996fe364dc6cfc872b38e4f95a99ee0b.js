1ca52a167f36089e8dffb7351da1a32b0ea123c4
Phuc Tran <glup3.tran@gmail.com>
2019-04-14 22:49:40 +0200

    Updates

---
+++ precache-manifest.996fe364dc6cfc872b38e4f95a99ee0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb64b1be3e1212e7f7d8ad4f1f1282ba",
+    "url": "/Glup3/portfolio-v2/static/media/Pikachu225.fb64b1be.jpg"
+  },
+  {
+    "revision": "0c0910ad7e979f670c52",
+    "url": "/Glup3/portfolio-v2/static/js/runtime~main.6ff4dac0.js"
+  },
+  {
+    "revision": "7759f313dfb1ece8a84b",
+    "url": "/Glup3/portfolio-v2/static/js/main.088f5178.chunk.js"
+  },
+  {
+    "revision": "c55489f1fcf708187ec9",
+    "url": "/Glup3/portfolio-v2/static/js/2.cc5476ec.chunk.js"
+  },
+  {
+    "revision": "7759f313dfb1ece8a84b",
+    "url": "/Glup3/portfolio-v2/static/css/main.2e283d50.chunk.css"
+  },
+  {
+    "revision": "c55489f1fcf708187ec9",
+    "url": "/Glup3/portfolio-v2/static/css/2.806d5799.chunk.css"
+  },
+  {
+    "revision": "25bc87486df302d0fad727da8a70810e",
+    "url": "/Glup3/portfolio-v2/index.html"
+  }
+];
