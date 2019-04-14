80e4642954f3b636bd8a4e62c95e61dc3ae60400
Phuc Tran <glup3.tran@gmail.com>
2019-04-14 20:37:26 +0200

    add social media icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import pikachu from '../images/Pikachu225.jpg';
+import IconsSocial from './IconsSocial';
 
 function Home() {
   return (
     <section>
       <div className="block container">
         <div className="center scrollspy" id="home">
-          <img src={pikachu} alt="profile" className="profile-pic"/>
+          <img src={pikachu} alt="profile" className="profile-pic z-depth-2"/>
           <h1>Hey, I'm Phuc!</h1>
           <div className="progress blue lighten-4">
               <div className="indeterminate light-blue darken-1"></div>
           </div>
           <h5>Full Stack Dev | Junior Developer | Unsatisfied Curiosity</h5>
+          <IconsSocial />
         </div>
       </div>
     </section>
